627ce5269f475f2bc62162ccad890dd8da4f99a9
Roger Fernandes <roger.osbourne@gmail.com>
2021-06-07 15:05:27 -0300

    Implement warning message to not founded makefile targets

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,23 @@
 #!/bin/sh -l
 
-make $1
+set -e
+
+function make_target_exists {
+    output=$(make -q "$1" 2>&1 | head -1)
+    [[ "$output" == "" ]]
+}
+
+function run_target {
+    echo Running $1...
+    if make_target_exists $1; then
+        make $1
+    else
+        echo "WARNING: Target [$1] not found in Makefile. This will be mandatory soon!"
+    fi
+}
+
+run_target lint
+
+run_target test
+
+run_target coverage
